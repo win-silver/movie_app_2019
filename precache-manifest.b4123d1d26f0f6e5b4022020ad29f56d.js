8c88ad87364236206d245b44dc7306eef924346a
연승은 <seungeun.yeon71@gmail.com>
2020-07-15 22:46:00 +0900

    Updates

---
+++ precache-manifest.b4123d1d26f0f6e5b4022020ad29f56d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a89d0e59e88bda143989e239d7e431",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "682d4672869a9cd280ea",
+    "url": "/movie_app_2019/static/css/main.55098598.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682d4672869a9cd280ea",
+    "url": "/movie_app_2019/static/js/main.192e1012.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
